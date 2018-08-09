610f6ddacf1dd3f1141a6ce75047b72f436c3faa
Sanjeet Verma <sanjeetverma@Sanjeets-Mac-mini.local>
2018-08-09 12:35:25 +0530

    initialcommit

---
+++ SwiftOcrDemo/OCR_ViewController.swift
@@ -0,0 +1,128 @@
+//
+//  ViewController.swift
+//  SwiftOcrDemo
+//
+//  Created by Sanjeet Verma on 08/08/18.
+//  Copyright © 2018 Sanjeet Verma. All rights reserved.
+//
+
+import UIKit
+import TesseractOCR
+import AVFoundation
+
+class OCR_ViewController: UIViewController {
+
+
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var topMarginConstraint: NSLayoutConstraint!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    @IBAction func btn_takephoto(_ sender: UIButton) {
+        presentImagePicker()
+       
+    }
+    
+    @IBAction func sliderValueDidChange(_ sender: UISlider) {
+        
+    }
+    
+    // Tesseract Image Recognition
+    func performImageRecognition(_ image: UIImage) {
+        
+        if let tesseract = G8Tesseract(language: "eng") {
+            tesseract.engineMode = .tesseractCubeCombined
+            tesseract.pageSegmentationMode = .auto
+            tesseract.image = image.g8_blackAndWhite()
+            tesseract.recognize()
+            textView.text = tesseract.recognizedText
+        }
+        activityIndicator.stopAnimating()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+// MARK: - UINavigationControllerDelegate
+extension OCR_ViewController: UINavigationControllerDelegate {
+}
+// MARK: - UIImagePickerControllerDelegate
+extension OCR_ViewController: UIImagePickerControllerDelegate {
+    func presentImagePicker() {
+        
+        let imagePickerActionSheet = UIAlertController(title: "Snap/Upload Image",
+                                                       message: nil, preferredStyle: .actionSheet)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraButton = UIAlertAction(title: "Take Photo",
+                                             style: .default) { (alert) -> Void in
+                                                let imagePicker = UIImagePickerController()
+                                                imagePicker.delegate = self
+                                                imagePicker.sourceType = .camera
+                                                self.present(imagePicker, animated: true)
+            }
+            imagePickerActionSheet.addAction(cameraButton)
+        }
+        
+        let libraryButton = UIAlertAction(title: "Choose Existing",
+                                          style: .default) { (alert) -> Void in
+                                            let imagePicker = UIImagePickerController()
+                                            imagePicker.delegate = self
+                                            imagePicker.sourceType = .photoLibrary
+                                            self.present(imagePicker, animated: true)
+        }
+        imagePickerActionSheet.addAction(libraryButton)
+        
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel)
+        imagePickerActionSheet.addAction(cancelButton)
+        
+        present(imagePickerActionSheet, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let selectedPhoto = info[UIImagePickerControllerOriginalImage] as? UIImage,
+            let scaledImage = selectedPhoto.scaleImage(640) {
+            
+            activityIndicator.startAnimating()
+            
+            dismiss(animated: true, completion: {
+                self.performImageRecognition(scaledImage)
+            })
+        }
+    }
+}
+
+// MARK: - UIImage extension
+extension UIImage {
+    func scaleImage(_ maxDimension: CGFloat) -> UIImage? {
+        
+        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+        
+        if size.width > size.height {
+            let scaleFactor = size.height / size.width
+            scaledSize.height = scaledSize.width * scaleFactor
+        } else {
+            let scaleFactor = size.width / size.height
+            scaledSize.width = scaledSize.height * scaleFactor
+        }
+        
+        UIGraphicsBeginImageContext(scaledSize)
+        draw(in: CGRect(origin: .zero, size: scaledSize))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaledImage
+    }
+}
+
